64915b76f3b6024449bdd19f40a6ec6a72d2cfd6
mireyalyons <90634951+mireyalyons@users.noreply.github.com>
2022-08-22 16:12:30 -0700

    Create Module 1 Quiz.sql

---
+++ Week 1/Module 1 Quiz.sql
@@ -0,0 +1,60 @@
+--1. Select the jobs below that may use SQL in their work (select all that apply).
+
+--QA Engineer
+--Data Scientist
+--DBA
+--Data Analyst
+--Backend Developer
+
+
+--2. How does a data scientist and DBA differ in how they use SQL?
+
+--DBAs manage the database for other users.
+
+
+--3. Which of the following statements are true of Entity Relationship (ER) Diagrams?
+
+--They show you the relationships between tables.
+--They identify the Primary Keys.
+--They usually are represented in a visual format. 
+--They are usually a representation of a business process.
+
+
+--4. Select the query below that will retrieve all columns from the customers table.
+
+--SELECT * FROM customers
+
+
+--5. Select the query that will retrieve only the Customer First Name, Last Name, and Company.
+
+SELECT 
+FirstName
+,LastName
+,Company
+FROM customers
+
+
+--6. The ER diagram below is depicting what kind of relationship between the EMPLOYEES and CUSTOMERS tables?
+
+-- One-to-Many
+
+
+--7. The data model depicted in the ER diagram below could be described as a _______________.
+
+-- Relational Model
+
+
+--8. When using the "CREATE TABLE" command and creating new columns for that table, which of the following statements is true?
+
+--You must  assign a data type to each column
+
+
+--9. Look at the values in the two columns below. Based on the values in each column, which column could potentially be used as a primary key?
+
+--Column 1
+
+
+--10. In order to retrieve data from a table with SQL, every SQL statement must contain?
+
+--SELECT
+
