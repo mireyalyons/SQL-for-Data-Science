badf0ebd56703014f1a4e80c9aac002901aff3fd
mireyalyons <90634951+mireyalyons@users.noreply.github.com>
2022-08-22 15:38:57 -0700

    Update Practice Quiz.sql

--- Week 1/Practice Quiz.sql
+++ Week 1/Practice Quiz.sql
@@ -1,4 +1,4 @@
-1. This statement will return an error. Please list why.
+--1. This statement will return an error. Please list why.
 
   SELECT
   TrackID
@@ -6,11 +6,36 @@
   AlbumID
   FROM tracks
   
-It lists too many columns
-It's missing comma after "TrackID", and "Name"
-It doesn't state where to get the data from 
+--It's missing comma after "TrackID", and "Name"
 
 
-Question 2
-In the ER diagram below, the infinity symbol is representing a "many" relationship and the key is representing "one". 
-Select all the tables that have a one-to-many relationship.
+--2. In the ER diagram below, the infinity symbol is representing a "many" relationship and the key is representing "one". 
+--Select all the tables that have a one-to-many relationship.
+
+--Artist to Albums
+--Customers to Invoices
+--Employees to Customers
+
+
+--3. When using SQLite, what datatypes can you assign to a column when creating a new table? Select all that apply.
+
+--Null
+--Integer
+--Text
+--Real
+
+
+--4. Primary Keys must be unique values.
+
+--True
+
+
+--5. What is the query below missing in order to execute?
+
+albums.title as album_title,
+tracks.name as track_name 
+FROM tracks 
+JOIN albums ON tracks.albumid = albums.albumid
+
+-- Select
+
